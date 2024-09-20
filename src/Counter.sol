1dc873ca65f15374396603847f5de3e8e97c52e3
beeb <703631+beeb@users.noreply.github.com>
2024-09-20 13:47:48 +0200

    demo with recursive deps

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IV4Router} from "v4-periphery/src/interfaces/IV4Router.sol";
+
 contract Counter {
     uint256 public number;
+    IV4Router internal immutable _router;
+
+    constructor() {
+        _router = IV4Router(address(0));
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -10,5 +17,9 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function routerInteraction() external pure {
+        revert IV4Router.V4TooLittleReceived(0, 0);
     }
 }
